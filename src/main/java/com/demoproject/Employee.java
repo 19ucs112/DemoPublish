1ae13727bc5e819dbeed4d63ed90d158426f2f42
Shaik <shaik.dastagiri@leegality.com>
2023-03-22 23:37:15 +0530

    initial commit

---
+++ src/main/java/com/demoproject/Employee.java
@@ -0,0 +1,22 @@
+package com.demoproject;
+
+public class Employee {
+    String name;
+    String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
